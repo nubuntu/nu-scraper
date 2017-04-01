b9351a4da8526ab08618a5d519b94787fdc7b5c0
Noercholis <poetnoer@gmail.com>
2017-04-01 10:49:38 +0700

    refactor

--- nu-scraper.js
+++ nu-scraper.js
@@ -1,13 +1,50 @@
-var nu          = require('nu-widget');
+require('nu-widget');
 var cheerio     = require('cheerio');
 var Horseman    = require('node-horseman');
-
+var express     = require('express');
+var fs          = require('fs')
 
 nu.widget('nubuntu.scraper', {
-    options: {},
+    options: {
+        port    :8080,
+        dir     :__dirname,
+        output  : 'array'
+    },
     _create : function(){
+        this.app    = express();
         this.engine = new Horseman();
-        this.engine = this.engine.open(this.options.url);
+        this._route();
+        this._listen();
+    },
+    _route  : function(){
+        var self = this;
+        this.app.use(function(req, res, next) {
+            self.uri        = req.url.split('/');
+            var file_name   = self.uri[1];
+            var file        = self.options.dir + '/' + file_name + '.js';
+            console.log('load script', file);
+            if (!fs.existsSync(file)) {
+                return res.send('Request ' + file_name + ' Not Found');
+            }else{
+                self.source = require(file).scraper;
+                self.res    = res;
+                return self._open();
+            }
+            return next();
+        });
+    },
+    _listen : function(){
+        var self = this;
+        this.app.listen(this.options.port,function(){
+            console.log("scraper engine ready!");
+            console.log('Listening on port %d',self.options.port);
+        });
+    },
+    _open        : function(){
+        var self = this;
+        this.engine.open(this.source.url).then(function(){
+            self.get_output();
+        });
     },
     _create_output : function(){
         switch(this.options.output){
@@ -21,19 +58,18 @@ nu.widget('nubuntu.scraper', {
     },
     _create_output_html : function(){
         this.output = this.$.hmtl();
+        this.res.send(this.output);
     },
     _create_output_array : function(){
         this.output = [];
-        if(this.options.rows){
+        if(this.source.rows){
             this.output = this._get_rows();
         }
-        if(this.options.output_callback){
-            this.options.output_callback.apply(this,[this.output]);
-        }
+        this.res.json(this.output);
     },
     _get_rows : function(){
         var self = this;
-        var rows = this.options.rows.apply(this,[this.$]);
+        var rows = this.source.rows.apply(this,[this.$]);
         var output_rows = [];
         if(rows.length<1)
             return output_rows;
@@ -45,18 +81,16 @@ nu.widget('nubuntu.scraper', {
     },
     _get_row : function($row){
         var fields = {};
-        if(!this.options.fields)
+        if(!this.source.fields)
             return fields;
-        for (var key in this.options.fields) {
-            var field   = this.options.fields[key];
+        for (var key in this.source.fields) {
+            var field   = this.source.fields[key];
             fields[key] = field.apply(this,[$row]);
         }
         return fields;
     },
-    get_array : function(callback){
+    get_output : function(){
         var self                        = this;
-        self.options.output             = 'array';
-        self.options.output_callback    = callback;
         this.engine.html().then(function(result){
             self.$       = cheerio.load(result);
             self._create_output();
